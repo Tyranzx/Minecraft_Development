73e2be0b8792476540dd800cbd3a8d5b39b29ef4
Tyranzx <70720366+Lewysan@users.noreply.github.com>
2023-09-02 11:51:45 -0700

    Create Reflection.java

---
+++ Reflection.java
@@ -0,0 +1,28 @@
+package empty;
+
+public class Reflection {
+
+  public static void main(String[] asd){
+    /*
+    Español:
+    La Reflection es una técnica que permite acceder y manipular dinámicamente las clases 
+    y métodos de Bukkit y sus complementos. Se utiliza para explorar la estructura interna de los 
+    objetos, como Class<?> y Constructor<?>, y realizar acciones como invocar métodos privados o 
+    modificar campos. Es útil para la creación de complementos flexibles y la interacción con código
+    externo de manera dinámica.
+    
+    Italiano:
+    La Reflection è una tecnica che consente di accedere e manipolare dinamicamente le classi 
+    e i metodi di Bukkit e dei suoi complementi. Viene utilizzata per esplorare la struttura interna degli 
+    oggetti, come Class<?> e Constructor<?>, e per eseguire azioni come l'invocazione di metodi privati
+    o la modifica di campi. È utile per la creazione di plugin flessibili e l'interazione dinamica con 
+    il codice esterno.
+    
+    English:
+    Reflection is a technique that allows dynamically accessing and manipulating Bukkit's 
+    classes and methods, as well as those from its plugins. It's used to explore the internal structure 
+    of objects, such as Class<?> and Constructor<?>, and perform actions like invoking private methods or 
+    modifying fields. It's valuable for creating flexible plugins and dynamically interacting with external code.
+        */
+  }
+}
