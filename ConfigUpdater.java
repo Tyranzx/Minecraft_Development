42f8c972144066c54aac8a4a2c43aac495f27724
Tyranzx <70720366+Lewysan@users.noreply.github.com>
2023-09-05 15:43:45 -0700

    Create ConfigUpdater.java

---
+++ ConfigUpdater.java
@@ -0,0 +1,33 @@
+package us.com.stellarsquad.stellarcraft.addons.config;
+
+import com.google.common.base.Preconditions;
+import org.bukkit.Bukkit;
+import org.bukkit.configuration.file.FileConfiguration;
+import org.bukkit.configuration.file.YamlConfiguration;
+import org.jetbrains.annotations.NotNull;
+import us.com.stellarsquad.stellarcraft.Loader;
+
+import java.io.File;
+import java.io.InputStreamReader;
+import java.nio.charset.StandardCharsets;
+
+// Should this work XD?
+
+public class ConfigUpdater
+{
+    public static FileConfiguration update(@NotNull Loader core, @NotNull String resource, @NotNull File toUpdate) {
+
+        Preconditions.checkArgument(toUpdate.exists(), "El archivo para actualizar no existe.");
+
+        FileConfiguration finalfc;
+
+        return toUpdate.exists() 
+                ? finalfc = YamlConfiguration.loadConfiguration(toUpdate) 
+                : YamlConfiguration.loadConfiguration(new InputStreamReader(core.getResource(resource), StandardCharsets.UTF_8));
+
+    }
+    /* @NotNull
+    private static int getVersion() {
+        return Integer.parseInt(Bukkit.getBukkitVersion().split("-")[0].split("\\.")[1]);
+    } */
+}
